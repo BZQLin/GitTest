70058ee62bb469c0c1146f814ccf6200d8ad41eb
BZQLin <393086306@qq.com>
2023-01-04 22:50:00 +0800

    test branch

--- runoob.php
+++ runoob.php
@@ -1,17 +1,3 @@
 <?php
-<<<<<<< HEAD
-echo 1;
+echo change_site;
 ?>
-$ git diff
-diff --git a/runoob.php b/runoob.php
-index e69de29..ac60739 100644
---- a/runoob.php
-+++ b/runoob.php
-@@ -0,0 +1,3 @@
-+<?php
-+echo 1;
-+?>
-=======
-echo 'runoob';
-?>
->>>>>>> change_site
